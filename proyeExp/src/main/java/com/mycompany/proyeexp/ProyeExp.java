f2bbaed71c1ca8499eaeb1d73b5360c42dd50a37
Fredd Badilla Víquez <freddalessandro2876@gmail.com>
2024-04-12 21:01:36 -0600

    Initial commit

---
+++ proyeExp/src/main/java/com/mycompany/proyeexp/ProyeExp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyeexp;
+
+/**
+ *
+ * @author fredd
+ */
+public class ProyeExp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
